d4740efbb996730a34ef09d590cd88cf13efc978
Replit user <>
2023-04-21 18:58:06 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -45,6 +45,11 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
+  if (searchTreeMap(tree, key) == NULL){
+    TreeNode *node = createTreeNode(key, value);
+    tree->current = node;
+  }
+  else return;
 
 }
 
