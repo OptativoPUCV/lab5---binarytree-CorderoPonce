777be72906d19ad9b1fcc5294838eafea92c88c7
Replit user <>
2023-04-21 19:36:25 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -52,7 +52,6 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     while(current != NULL){
       if(tree->lower_than(key, current->pair->key)){
         if (current->left == NULL){
-          current->left->parent = current;
           current->left = node;
           return;  
         }
@@ -60,7 +59,6 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
       }
       else{
         if (current->right == NULL){
-          current->right->parent = current;
           current->right = node;
           return;
         }
